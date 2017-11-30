e7568d277b2f3a6de549ec20dd6b400d0288641d
Shubham Maurya <code.shaurya@gmail.com>
2017-11-30 23:02:17 +0530

    Logistic map is applied

--- encryption.cpp
+++ encryption.cpp
@@ -7,12 +7,24 @@ using namespace std;
 int main()
 {
     Mat image;
+    double x[102];
+    double u;  //u is the control parameter for chaotic map,also known as population rate
+
     image = imread( "Image.jpg", 1 );
     if ( !image.data )
     {
         printf("No image data \n");
         return -1;
     }
+    //Applying Logistic map
+    u=3.94;   //It is the condition for logistic map
+    x[0]=0.4; //Base condition for logistic map
+    for (int i = 1; i <= 100; ++i)
+        x[i]=u*x[i-1]*(1-x[i-1]);
+    
+    for (int i = 0; i<=100; ++i)
+        cout<<x[i]<<" ";
+
     namedWindow("Shubham@shaurya", WINDOW_AUTOSIZE );
     imshow("Shubham@shaurya", image);
     waitKey(0);
